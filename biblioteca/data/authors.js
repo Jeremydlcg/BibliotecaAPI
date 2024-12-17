f29ac65126c27344899da35332c9885837f42180
jeremydlcg <jeremydlcg@gmail.com>
2024-12-16 21:53:22 -0400

    Biblioteca

---
+++ biblioteca/data/authors.js
@@ -0,0 +1,14 @@
+const authors = [
+  {
+    id: 1,
+    name: "George Orwell",
+    nationality: "British"
+  },
+  {
+    id: 2,
+    name: "Gabriel García Márquez",
+    nationality: "Colombian"
+  }
+];
+
+module.exports = authors;
---
+++ biblioteca/data/books.js
@@ -0,0 +1,25 @@
+const books = [
+  {
+    id: 1,
+    title: "1984",
+    authorId: 1,
+    year: 1949,
+    available: true
+  },
+  {
+    id: 2,
+    title: "Animal Farm",
+    authorId: 1,
+    year: 1945,
+    available: false
+  },
+  {
+    id: 3,
+    title: "One Hundred Years of Solitude",
+    authorId: 2,
+    year: 1967,
+    available: true
+  }
+];
+
+module.exports = books;
